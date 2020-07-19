0eb6ed51510ee712e2b72eb5ff539ed222b29421
sulincix <parduscix@yandex.ru>
2020-07-19 18:10:09 +0000

    netinst fix

--- netinst.sh
+++ netinst.sh
@@ -3,4 +3,4 @@ cd $tmpdir
 wget https://gitlab.com/sulinos/devel/unibuild/-/archive/master/unibuild-master.tar.gz -O unibuild.tar.gz
 tar -xf unibuild.tar.gz
 cd unibuild-master
-su -c "bash install.sh"
+bash install.sh
--- readme.rst
+++ readme.rst
@@ -4,7 +4,9 @@ The universal **build** script system.
 
 Easy install
 ============
+
 .. code-block:: shell
+
 	curl -s https://gitlab.com/sulinos/devel/unibuild/-/raw/master/netinst.sh | bash -s --
 
 for install
