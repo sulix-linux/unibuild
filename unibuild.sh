e0587efc78609d0f064787aefcecef67efafbc45
sulincix <parduscix@yandex.ru>
2020-07-20 09:29:16 +0000

    api created

--- unibuild.sh
+++ unibuild.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 [ "$MODDIR" == "" ] && export MODDIR=/usr/lib/unibuild/modules
 set -e
+for api in $(ls $MODDIR/../api | sort) ; do
+	source $MODDIR/../api/$api
+done
 for mod in $(ls $MODDIR) ; do
-	echo "Loading: $mod"
 	source $MODDIR/$mod
 done
 if [ -f "$1" ] ; then
@@ -15,17 +17,26 @@ else
 fi
 source $MODDIR/../target/$TARGET
 source $MODDIR/../host/$HOST
+msg ">>> Checking dependencies"
 _get_build_deps
 cd $BUILDDIR
+msg ">>> Getting sources"
 _fetch
 cd $WORKDIR
+msg ">>> Running setup function"
 _setup
 cd $WORKDIR
+msg ">>> Running build function"
 _build
 cd $WORKDIR
+msg ">>> Running install function"
 _install
 cd $WORKDIR
+msg ">>> Generating metadata"
 _create_metadata
 cd $WORKDIR
+msg ">>> Creating package"
 _package
+msg ">>> Clearing workdir"
 rm -rf $WORKDIR
+info ">>> Done"
